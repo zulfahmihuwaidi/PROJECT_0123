42135459387ab695fced4765f3a706b9f1c1fbe4
zulfahmihuwaidi <muhammad.huwaidi@students.amikom.ac.id>
2018-12-17 16:41:58 +0700

    Fikasasi File

--- src/mataUang/KonversiRupiah.java
+++ src/mataUang/KonversiRupiah.java
@@ -6,13 +6,19 @@ public class KonversiRupiah {
 	public static void main(String[] args) {
         Scanner masukan = new Scanner(System.in);
         float rupiah, dolar;
-        System.out.print("Masukan Jumlah Rupiah = ");
+        
+        System.out.println("Masukan Jumlah Rupiah dan Dolar = ");
+     
         rupiah = masukan.nextFloat();
+        dolar = masukan.nextFloat();
+        
+        rupiah = (float) (dolar*14000);
         dolar = (float) (rupiah/14000);
         
         System.out.println();
         System.out.println("Hasil Konversi = ");
         System.out.println("Dolar = " + dolar);
+        System.out.println("Rupiah = " + rupiah);
 	}
 	
 }
