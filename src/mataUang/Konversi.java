ad86a70b56ad4cbeb584332133aeaa69d96b1433
zulfahmihuwaidi <muhammad.huwaidi@students.amikom.ac.id>
2018-12-17 16:09:06 +0700

    Membuat File Java

---
+++ src/mataUang/Konversi.java
@@ -0,0 +1,19 @@
+package mataUang;
+
+import java.util.Scanner;
+
+public class Konversi {
+	public static void main(String[] args) {
+        Scanner masukan = new Scanner(System.in);
+        float rupiah, dolar, euro;
+        System.out.print("Masukan Rupiah = ");
+        rupiah = masukan.nextFloat();
+        dolar = (float) (rupiah*0.0001);
+        euro = (float) (rupiah*0.00004);
+        System.out.println();
+        System.out.println("Hasil Kurs = ");
+        System.out.println("Dolar = " + dolar);
+        System.out.println("Euro = " + euro);
+	}
+	
+}
