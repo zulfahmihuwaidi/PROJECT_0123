803879aa120a66f228a4b15c1f0b90d8bfa285ca
zulfahmihuwaidi <muhammad.huwaidi@students.amikom.ac.id>
2018-12-17 16:24:05 +0700

    Merapihkan Srcipt pada file Konversi

--- src/mataUang/Konversi.java
+++ src/mataUang/Konversi.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Konversi {
 	public static void main(String[] args) {
         Scanner masukan = new Scanner(System.in);
-        float rupiah, dolar, euro;
-        System.out.print("Masukan Rupiah = ");
+        float rupiah, dolar;
+        System.out.print("Masukan Jumlah Rupiah = ");
         rupiah = masukan.nextFloat();
         dolar = (float) (rupiah/14000);
         
         System.out.println();
-        System.out.println("Hasil Kurs = ");
+        System.out.println("Hasil Konversi = ");
         System.out.println("Dolar = " + dolar);
 	}
 	
